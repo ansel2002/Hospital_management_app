deb77b56339d06544d9b535e92eb5f402bf9bfaf
Ardra K S <ardra.ks@msds.christuniversity.in>
2024-01-06 23:56:57 +0530

    updated cart

--- Pharmacy/urls.py
+++ Pharmacy/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
 from .import views
-from .views import about
+from .views import about,cart
 
 urlpatterns = [
     path('',about,name="dashboard"),
     path('about',about,name="about"),
+    path('cart',cart,name="cart"),
 ]
 
--- Pharmacy/views.py
+++ Pharmacy/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def about(request):
     return render(request,"Pharmacy/about.html",context={})
+
+def cart(request):
+    return render(request,"Pharmacy/cart.html",context={})
