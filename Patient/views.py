d16637770c62426d64319cfb2b51fcce49843853
vikash shakya <itsdarkethan@gmail.com>
2024-01-29 10:06:48 +0530

    update

--- Patient/views.py
+++ Patient/views.py
@@ -15,65 +15,10 @@ from django.db.models import Q
 import logging
 import os
 
-def patient_login_view(request):
-    return render(request, 'patientlogin.html')
 
 def patient_home(request):
     return render(request, 'patient/patient_home.html')
 
-
-
-def is_admin(user):
-    return user.groups.filter(name='ADMIN').exists()
-def is_doctor(user):
-    return user.groups.filter(name='DOCTOR').exists()
-def is_patient(user):
-    return user.groups.filter(name='PATIENT').exists()
-
-def patientclick_view(request):
-    try:
-        if request.user.is_authenticated:
-            return HttpResponseRedirect('afterlogin')
-        return render(request,'patient/patientclick.html')
-    except Exception as e:
-        logging.error("error in patientclick_view,error is {}".format(e)) 
-        return render(request,'patient/patientclick.html')  
-    
-def patient_signup_view(request):
-    try:
-        userForm=forms.PatientUserForm()
-        patientForm=forms.PatientForm()
-        mydict={'userForm':userForm,'patientForm':patientForm}
-        if request.method=='POST':
-            userForm=forms.PatientUserForm(request.POST)
-            patientForm=forms.PatientForm(request.POST,request.FILES)
-            if userForm.is_valid() and patientForm.is_valid():
-                user=userForm.save()
-                user.set_password(user.password)
-                user.save()
-                patient=patientForm.save(commit=False)
-                patient.user=user
-                patient.assignedDoctorId=request.POST.get('assignedDoctorId')
-                patient.isDischarged=False
-                patient=patient.save()
-                my_patient_group = Group.objects.get_or_create(name='PATIENT')
-                my_patient_group[0].user_set.add(user)
-                return HttpResponseRedirect('patientlogin')
-            else:
-                messages.error(request,"Patient User Exist.")
-                logging.error("error in patient_signup_view with data already exist.")    
-    except Exception as e:
-        logging.error("error in patient_signup_view, error is {}".format(e))
-        messages.error(request, "There was an internel server error")
-        return HttpResponseRedirect('patientlogin')
-
-    return render(request,'hospital/patientsignup.html',context=mydict)
-
-#---------------------------------------------------------------------------------
-#------------------------ PATIENT RELATED VIEWS START ------------------------------
-#---------------------------------------------------------------------------------
-@login_required(login_url='patientlogin')
-@user_passes_test(is_patient)
 def patient_dashboard_view(request):
     patient=models.Patient.objects.get(user_id=request.user.id)
     doctor=models.Doctor.objects.get(user_id=patient.assignedDoctorId)
@@ -91,20 +36,11 @@ def patient_dashboard_view(request):
     }
     return render(request,'patient/patient_dashboard.html',context=mydict)
 
-def some_function():
-    REST_API_PORT = 8000
-    url = f'http://localhost:{REST_API_PORT}/transactions'
 
-
-@login_required(login_url='patientlogin')
-@user_passes_test(is_patient)
 def patient_appointment_view(request):
     patient=models.Patient.objects.get(user_id=request.user.id) #for profile picture of patient in sidebar
     return render(request,'patient/patient_appointment.html',{'patient':patient})
 
-
-@login_required(login_url='patientlogin')
-@user_passes_test(is_patient)
 def patient_book_appointment_view(request):
     appointmentForm=forms.PatientAppointmentForm()
     patient=models.Patient.objects.get(user_id=request.user.id) #for profile picture of patient in sidebar
@@ -187,15 +123,12 @@ def search_doctor_view(request):
     doctors=models.Doctor.objects.all().filter(status=True).filter(Q(department__icontains=query)| Q(user__first_name__icontains=query))
     return render(request,'patient/patient_view_doctor.html',{'patient':patient,'doctors':doctors})
 
-@login_required(login_url='patientlogin')
-@user_passes_test(is_patient)
+
 def patient_view_appointment_view(request):
     patient=models.Patient.objects.get(user_id=request.user.id) #for profile picture of patient in sidebar
     appointments=models.Appointment.objects.all().filter(patientId=request.user.id)
     return render(request,'patient/patient_view_appointment.html',{'appointments':appointments,'patient':patient})
 
-@login_required(login_url='patientlogin')
-@user_passes_test(is_patient)
 def patient_discharge_view(request):
     patient=models.Patient.objects.get(user_id=request.user.id) #for profile picture of patient in sidebar
     patientDict=None
@@ -231,8 +164,6 @@ def patient_discharge_view(request):
     return render(request,'patient/patient_discharge.html',context=patientDict)
 
 
-@login_required(login_url='patientlogin')
-@user_passes_test(is_patient)
 def update_patient_patient_view(request,pk):
     patient=models.Patient.objects.get(id=pk)
     user=models.User.objects.get(id=patient.user_id)
